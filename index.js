5dfc595138a6f1ae064b5e5c8e68dc7240d5b187
Mr.Spoon <silence1198@outlook.com>
2022-04-25 13:00:43 -0400

    update settings

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ import "react-native-gesture-handler"
 LogBox.ignoreLogs([
     "Can't perform a React state update on an unmounted component. This is a no-op, but it indicates a memory leak in your application. To fix, cancel all subscriptions and asynchronous tasks in a useEffect cleanup function."
 ])
+LogBox.ignoreLogs([
+    "[react-native-gesture-handler] Seems like you\'re using an old API with gesture components, check out new Gestures system!",
+  ]);
 // LogBox.ignoreAllLogs(true)
 
 AppRegistry.registerComponent(appName, () => App);
